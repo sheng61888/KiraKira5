1f3770da0573a294bf753c63e43432de18857263
Sam Kian Sheng <ggsheng2727@gmail.com>
2025-11-07 22:16:21 +0800

    dotnet

--- code/connect_db.cs
+++ code/connect_db.cs
@@ -1,1 +1,27 @@
-// Connect to database
+using MySql.Data.MySqlClient;
+
+public class DatabaseConnection
+{
+    private static string connectionString = "Server=0.tcp.ap.ngrok.io;Port=12214;Database=kirakiradb;Uid=root;Pwd=;";
+    
+    public static MySqlConnection GetConnection()
+    {
+        return new MySqlConnection(connectionString);
+    }
+    
+    public static bool TestConnection()
+    {
+        try
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                return true;
+            }
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
