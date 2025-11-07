870a11e292bc5223e525dad91b9e24edb54a8139
Sam Kian Sheng <ggsheng2727@gmail.com>
2025-11-07 10:57:58 +0800

    h

---
+++ js/script.js
@@ -0,0 +1,28 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.getElementById('mobile-menu-button');
+    const closeButton = document.getElementById('mobile-menu-close');
+    const mobileMenu = document.getElementById('mobile-menu');
+    const overlay = document.getElementById('overlay');
+    const body = document.body;
+
+    function openMenu() {
+        mobileMenu.classList.add('open');
+        overlay.classList.add('open');
+        body.classList.add('body-no-scroll'); // Prevent scrolling
+    }
+
+    function closeMenu() {
+        mobileMenu.classList.remove('open');
+        overlay.classList.remove('open');
+        body.classList.remove('body-no-scroll'); // Allow scrolling
+    }
+
+    menuButton.addEventListener('click', openMenu);
+    closeButton.addEventListener('click', closeMenu);
+    overlay.addEventListener('click', closeMenu);
+
+    // Close menu when clicking a link inside it
+    mobileMenu.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', closeMenu);
+    });
+});
