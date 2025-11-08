c8c03126e3aeb3b30b1e70918f89df2c40db625b
sheng <ggsheng2727@gmail.com>
2025-11-08 23:10:05 +0800

    d

--- database_query/create_user2.sql
+++ database_query/create_user2.sql
@@ -1,5 +1,9 @@
 CREATE USER 'user_kulsum'@'%' IDENTIFIED BY 'kulsum123';
+CREATE USER 'user_arielle'@'%' IDENTIFIED BY 'arielle123';
 
 GRANT ALL PRIVILEGES ON kirakiradb.* TO 'user_kulsum'@'%';
+GRANT ALL PRIVILEGES ON kirakiradb.* TO 'user_arielle'@'%';
 
 FLUSH PRIVILEGES;
+
+SELECT User, Host FROM mysql.user;
--- obj/Debug/net9.0/KiraKira5.AssemblyInfo.cs
+++ obj/Debug/net9.0/KiraKira5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KiraKira5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9fd222f99e2577d6d543a4ed1d793d64574472c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66ad1baf1bff337a0aeb501f482c0ae4e5955511")]
 [assembly: System.Reflection.AssemblyProductAttribute("KiraKira5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KiraKira5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
