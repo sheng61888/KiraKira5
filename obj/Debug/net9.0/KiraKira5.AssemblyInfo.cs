95b3e2ca2975cdeb088effbbb378f17186cee3f1
sheng <ggsheng2727@gmail.com>
2025-11-09 20:01:19 +0800

    pyp and trials

--- obj/Debug/net9.0/KiraKira5.AssemblyInfo.cs
+++ obj/Debug/net9.0/KiraKira5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KiraKira5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da2a6e13563b9311b0559e73045d7d66ea3e0df5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9167dec02cd4ab6e917f64ba145ac9578d33fb6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("KiraKira5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KiraKira5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
