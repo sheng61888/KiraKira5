29ab25c7122b3bd6886594c198cbbac0dfc6ade1
sheng <ggsheng2727@gmail.com>
2025-11-10 10:02:11 +0800

    s

--- obj/Debug/net9.0/KiraKira5.AssemblyInfo.cs
+++ obj/Debug/net9.0/KiraKira5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KiraKira5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9366ac4fce69f6ad636134c4f67fdee62eaa4d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8e8fb431c5abd87be2bfa965cc9116bdb774c8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("KiraKira5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KiraKira5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
