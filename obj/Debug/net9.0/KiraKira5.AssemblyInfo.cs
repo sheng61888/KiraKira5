f8e1661e6e58b14c607acbb73d4a1061bc318acd
Teoh781 <zhexiang05@gmail.com>
2025-11-12 21:19:37 +0800

    .

--- obj/Debug/net9.0/KiraKira5.AssemblyInfo.cs
+++ obj/Debug/net9.0/KiraKira5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KiraKira5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32583ae50a111af6d4defb2df3890251cec241b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b464408a14bbae0ee7de1c325029c3c60f6cd051")]
 [assembly: System.Reflection.AssemblyProductAttribute("KiraKira5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KiraKira5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
