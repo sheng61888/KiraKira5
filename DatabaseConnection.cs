6c1215e23e7fc31f0b76525602ed62587169e67a
sheng <ggsheng2727@gmail.com>
2025-11-13 23:34:49 +0800

    11431

--- DatabaseConnection.cs
+++ DatabaseConnection.cs
@@ -2,7 +2,7 @@ using MySql.Data.MySqlClient;
 
 public class DatabaseConnection
 {
-    private static string connectionString = "Server=0.tcp.ap.ngrok.io;Port=12685;Database=kirakiradb;Uid=root;Pwd=Sheng#0618;";
+    private static string connectionString = "Server=0.tcp.ap.ngrok.io;Port=11431;Database=kirakiradb;Uid=root;Pwd=Sheng#0618;";
     
     public static MySqlConnection GetConnection()
     {
--- obj/Debug/net9.0/KiraKira5.AssemblyInfo.cs
+++ obj/Debug/net9.0/KiraKira5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KiraKira5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21b0d8d7fe037691b1892d82e321ef96cb538a9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8db789a58d0c629fce7e134361b209fe67c46f41")]
 [assembly: System.Reflection.AssemblyProductAttribute("KiraKira5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KiraKira5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
